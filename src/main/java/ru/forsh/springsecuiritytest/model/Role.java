190df22f091a582483b77ca29a1b0978d2241e12
Nikolay <forsh_nikolay@mail.ru>
2022-04-06 15:56:10 +0300

    add granted authorities according to roles

--- src/main/java/ru/forsh/springsecuiritytest/model/Role.java
+++ src/main/java/ru/forsh/springsecuiritytest/model/Role.java
@@ -1,6 +1,28 @@
 package ru.forsh.springsecuiritytest.model;
 
-public enum Role {
-    USER,
-    ADMIN
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public enum Role  {
+    USER(Set.of(Permission.DEVELOPERS_READ)),
+    ADMIN(Set.of(Permission.DEVELOPERS_WRITE, Permission.DEVELOPERS_READ));
+
+    private final Set<Permission> permissions;
+
+    Role(Set<Permission> permissions) {
+        this.permissions = permissions;
+    }
+
+    public Set<Permission> getPermissions() {
+        return permissions;
+    }
+
+    // Создаём метод, который преобразует наши Permission в
+    public Set<SimpleGrantedAuthority> getAuthorities(){
+        return getPermissions().stream()
+                .map(permission -> new SimpleGrantedAuthority(permission.getPermission()))
+                .collect(Collectors.toSet());
+    }
 }
